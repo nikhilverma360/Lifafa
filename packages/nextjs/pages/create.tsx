906b150247e6af23ff02fb30a6bb3dd10d4b172c
Nikhil Verma <nv5475609@gmail.com>
2023-10-20 00:13:31 +0530

    react router setup

---
+++ packages/nextjs/pages/create.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Create: NextPage = () => {
+  return <>Create</>;
+};
+
+export default Create;
---
+++ packages/nextjs/pages/manage.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Manage: NextPage = () => {
+  return <>Manage</>;
+};
+
+export default Manage;
