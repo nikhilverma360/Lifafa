ce4d3051be67eb33ff6b3221e54c31f5b7b8da2d
Nikhil Verma <nv5475609@gmail.com>
2023-10-20 12:37:51 +0530

    create

--- packages/nextjs/pages/create.tsx
+++ packages/nextjs/pages/create.tsx
@@ -1,7 +1,24 @@
+import { Dispatch, SetStateAction } from "react";
+import { AbiParameter } from "abitype";
 import type { NextPage } from "next";
+import { InputBase } from "~~/components/scaffold-eth";
+
+type ContractInputProps = {
+  setForm: Dispatch<SetStateAction<Record<string, any>>>;
+  form: Record<string, any> | undefined;
+  stateObjectKey: string;
+  paramType: AbiParameter;
+};
+
 
 const Create: NextPage = () => {
-  return <>Create</>;
+  
+
+  return <>
+  <div>
+    hi
+  </div>
+  </>;
 };
 
 export default Create;
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,11 +1,27 @@
 //import Link from "next/link";
 import type { NextPage } from "next";
 import { MetaHeader } from "~~/components/MetaHeader";
+import TypewriterComponent from "typewriter-effect";
 
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
+      <div className=" fixed top-40 left-40 text-8xl">
+      <div className="text-transparent pt-8 bg-clip-text bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-900 via-gray-600 to-blue-400 animate-text">
+          <TypewriterComponent
+            options={{
+              strings: [
+                "Lifafa.xyz",
+                "Red Envelope",
+                "Claim digital Asset",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+      </div>
     </>
   );
 };
