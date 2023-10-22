e6eb6ba34b2a35c3140b934209a989c76db3f326
Nikhil Verma <nv5475609@gmail.com>
2023-10-22 21:35:09 +0530

    fix boiler plate for sismo ingtegration

--- packages/nextjs/pages/claim/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-
-const Claim: NextPage = () => {
-  return <>Claim</>;
-};
-
-export default Claim;
--- packages/nextjs/pages/manage.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-
-const Manage: NextPage = () => {
-  return <>Manage</>;
-};
-
-export default Manage;
